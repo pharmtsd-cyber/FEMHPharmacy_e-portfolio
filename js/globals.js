e4b780d8d25b6e2372173ccad8edcaacf2ecc92f
pharmachris <pharm.tsd@gmail.com>
2026-09-18 10:31:31 +0800

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -5,12 +5,12 @@ let currentRecordId = "";
 let currentSavedAnswers = {}; 
 let globalUserList = []; 
 let globalTasks = []; 
+// 💡 已將 obs 移除，只保留評核(ass)與雙向回饋(fb)
 let timerStates = { 
-  obs: { isRunning: false, start: null, elapsed: 0 }, 
   ass: { isRunning: false, start: null, elapsed: 0 }, 
   fb:  { isRunning: false, start: null, elapsed: 0 } 
 };
-let timerRaf = {}; // 用於優化計時器的 requestAnimationFrame
+let timerRaf = {}; 
 let autoSaveInterval = null; 
 let canvases = {};
 let chartAcgmeInstance = null;
