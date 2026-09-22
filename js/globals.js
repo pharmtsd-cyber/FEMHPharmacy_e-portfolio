aca1b702d8cd045ce267334c205b4766d5475045
pharmachris <pharm.tsd@gmail.com>
2026-09-22 15:42:52 +0800

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -11,6 +11,7 @@ let globalHistoryCounts = {};
 
 // 🌟 新增：全域題庫與快取判定變數
 let globalQuestions = []; 
+let globalDopsQuestions = []; // 🌟 新增：專門存放 DOPS 公版題目的快取
 let isDashboardLoaded = false; 
 
 let timerStates = { 
