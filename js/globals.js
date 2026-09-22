fde4999b98277a50a640d77873968b818668e5f7
pharmachris <pharm.tsd@gmail.com>
2026-09-22 14:47:00 +0800

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -3,15 +3,19 @@ let allTemplates = [];
 let currentTemplateId = ""; 
 let currentRecordId = ""; 
 let currentAttemptCount = 0; 
-let currentTaskStatus = ""; // 🌟 新增：記錄目前的任務狀態(待回填/草稿)
+let currentTaskStatus = ""; 
 let currentSavedAnswers = {}; 
 let globalUserList = []; 
 let globalTasks = []; 
 let globalHistoryCounts = {}; 
 
+// 🌟 新增：全域題庫與快取判定變數
+let globalQuestions = []; 
+let isDashboardLoaded = false; 
+
 let timerStates = { 
   ass: { isRunning: false, start: null, elapsed: 0 },
-  fb:  { isRunning: false, start: null, elapsed: 0 } // 🌟 補回 DOPS 雙向回饋計時器
+  fb:  { isRunning: false, start: null, elapsed: 0 } 
 };
 let timerRaf = {}; 
 let autoSaveInterval = null; 
